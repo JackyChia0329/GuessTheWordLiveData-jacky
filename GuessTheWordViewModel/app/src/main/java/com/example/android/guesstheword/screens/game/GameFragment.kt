694933bfeb9d396417354e7cd282389933d420f4
jackychia0329 <chiayj-am17@student.tarc.edu.my>
2019-12-17 13:50:38 +0800

    jacky

--- GuessTheWordViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ GuessTheWordViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -24,11 +24,13 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
-
+import androidx.navigation.fragment.NavHostFragment.findNavController
 /**
  * Fragment where the game is played
  */
@@ -56,8 +58,16 @@ class GameFragment : Fragment() {
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
         binding.endGameButton.setOnClickListener { onEndGame() }
-        updateScoreText()
-        updateWordText()
+
+        viewModel.eventGameFinish.observe(this, Observer<Boolean> { hasFinished ->
+            if (hasFinished) gameFinished()
+        })
+        viewModel.score.observe(this, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+        viewModel.word.observe(this, Observer { newWord ->
+            binding.wordText.text = newWord
+        })
         return binding.root
     }
 
@@ -65,13 +75,11 @@ class GameFragment : Fragment() {
 
     private fun onSkip() {
         viewModel.onSkip()
-        updateWordText()
-        updateScoreText()
+
     }
     private fun onCorrect() {
         viewModel.onCorrect()
-        updateScoreText()
-        updateWordText()
+
     }
 
     private fun onEndGame() {
@@ -80,22 +88,14 @@ class GameFragment : Fragment() {
 
     /** Methods for updating the UI **/
 
-    private fun updateWordText() {
-        binding.wordText.text = viewModel.word
-
-    }
-
-    private fun updateScoreText() {
-        binding.scoreText.text = viewModel.score.toString()
-    }
-
     /**
      * Called when the game is finished
      */
     private fun gameFinished() {
         Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
         val action = GameFragmentDirections.actionGameToScore()
-        action.score = viewModel.score
-        NavHostFragment.findNavController(this).navigate(action)
+        action.score = viewModel.score.value?:0
+        findNavController(this).navigate(action)
+        viewModel.onGameFinish()
     }
 }
--- GuessTheWordViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWordViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -17,19 +17,27 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 /**
  * ViewModel containing all the logic needed to run the game
  */
 class GameViewModel : ViewModel() {
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+    get() = _eventGameFinish
 
     // The current word
-    var word = ""
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+    get() = _word
 
     // The current score
-    var score = 0
-
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+    get() = _score
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
@@ -65,6 +73,8 @@ class GameViewModel : ViewModel() {
     }
 
     init {
+        _word.value=""
+        _score.value = 0
         Log.i("GameViewModel", "GameViewModel created!")
         resetList()
         nextWord()
@@ -81,13 +91,13 @@ class GameViewModel : ViewModel() {
     /** Methods for updating the UI **/
     fun onSkip() {
         if (!wordList.isEmpty()) {
-            score--
+            _score.value = (score.value)?.minus(1)
         }
         nextWord()
     }
     fun onCorrect() {
         if (!wordList.isEmpty()) {
-            score++
+            _score.value =(score.value)?.plus(1)
         }
         nextWord()
     }
@@ -98,7 +108,18 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (!wordList.isEmpty()) {
-            word = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
+
+        }else {
+            //select and remove a _word from list
+            _eventGameFinish.value=true
+            onGameFinish()
         }
     }
+    fun onGameFinish(){
+        _eventGameFinish.value = false
+    }
+//    fun onGameFinishComplete(){
+//        _eventGameFinish.value=false
+//    }
 }
